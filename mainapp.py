60159c0428689c8f98b41582b6019d26da7f2af8
ChatPatent <134037708+ChatPatent@users.noreply.github.com>
2023-09-12 17:13:56 +0800

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -24,10 +24,9 @@ index = VectorstoreIndexCreator().from_loaders([loaders])
 st.title('✨ AI Smart Query | AI智慧畅聊 ')
 prompt = st.text_input("Welcome to AI Smart Query, questions will be answered based on the sources in our docs directory.\n\n欢迎来到AI智慧畅聊，基于本项目docs目录下的文档资源进行高效QA问答。")
 
-if prompt:
+if prompt!="" and not prompt.strip().isspace() and not prompt == "" and not prompt.strip() == "" and not prompt.isspace():    
     # stuff chain type sends all the relevant text chunks from the document to LLM    
     response = index.query(llm=llm, question = prompt, chain_type = 'stuff')    
-
     # Write the results from the LLM to the UI
     st.write("<br><i>" + response + "</i><hr>", unsafe_allow_html=True )
     #st.write("<b>" + prompt + "</b><br><i>" + response + "</i><hr>", unsafe_allow_html=True )
